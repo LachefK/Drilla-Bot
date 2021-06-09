b60a93fba87cc53bee8a30723fec56422a3725a4
LachefK <85521768+LachefK@users.noreply.github.com>
2021-06-08 21:45:00 -0300

    Update daftarvip.js

--- src/daftarvip.js
+++ src/daftarvip.js
@@ -3,14 +3,11 @@ const daftarvip = (prefix) => {
 	
 *PREÇO DE LISTA VIP :*
 
--Rp. 10 reais> Acessar recursos ViP
--Rp. 30 reais> Recursos VIP + 1 bot editavel pra vc
-
-*SE QUER REGISTAR VIP :*
+-Rp. 35 reais> Acessar recursos ViP por 10 segundos
 
 *Proprietário do BOT :*
 
-_wa.me/558892594715 ou digite *${prefix}owner*_
+_wa.me/5521969395519 ou digite *${prefix}owner*_
 
 
 `

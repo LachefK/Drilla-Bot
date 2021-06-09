f79ffa199005987a130dc153b837b078c4bc76c6
LachefK <85521768+LachefK@users.noreply.github.com>
2021-06-08 21:52:12 -0300

    Update metodos.js

--- src/metodos.js
+++ src/metodos.js
@@ -1,13 +1,13 @@
 const metodos = (prefix, sender) => {
-	return `PRIVATE METODOS VIP
+	return `DRILLA METODOS VIP
 
-*${prefix}mt1* Painel Kiny
+*${prefix}mt1* Em breve
 *${prefix}mt2* Em breve
 *${prefix}mt3* Em breve
 *${prefix}mt4* Em breve
 *${prefix}mt5* Em breve
 
-METODOS AGREGADOS POR SKILLER 
+METODOS AGREGADOS POR KARIB 
 ` 
 }
 
